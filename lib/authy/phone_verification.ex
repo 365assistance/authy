fa9e43128d96cec9cef654b0ed8550ac4a979962
Brenton Annan <brentonannan@brentonannan.com>
2016-05-29 18:53:25 +1000

    PhoneVerification module

---
+++ lib/authy/phone_verification.ex
@@ -0,0 +1,9 @@
+defmodule Authy.PhoneVerification do
+  @moduledoc """
+  Access to the Authy Phone Verification API
+
+  See: https://docs.authy.com/phone_verification.html
+  """
+
+  @base_url "/protected/json/phones/verification"
+end
---
+++ test/authy/phone_verification_test.exs
@@ -0,0 +1,4 @@
+defmodule Authy.PhoneVerificationTest do
+  use ExUnit.Case, async: true
+  doctest Authy.PhoneVerification
+end
