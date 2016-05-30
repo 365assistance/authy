1274e7e9c4e654fb1e830fe76ba9cff0c420f715
Brenton Annan <brentonannan@brentonannan.com>
2016-05-30 13:12:42 +1000

    Implementation of PhoneVerification.start

--- lib/authy/phone_verification.ex
+++ lib/authy/phone_verification.ex
@@ -18,6 +18,27 @@ defmodule Authy.PhoneVerification do
 
   @base_url "/protected/json/phones/verification"
 
+  @doc """
+  Request a verification token be sent to a user
+
+  params map must include :phone_number
+  :via and :country_code are also required but may be specified in config
+  :locale and :custom_message are optional and may be specified in config
+
+  post_fn replaces the actual call to Authy.post
+  """
+  def start(params, post_fn \\ &post_start/1)
+  def start(params = %{phone_number: _}, post_fn) do
+    params
+    |> set_defaults
+    |> Map.take([:via, :phone_number, :country_code, :locale, :custom_message])
+    |> post_fn.()
+  end
+
+  defp post_start(params = %{via: via, phone_number: _, country_code: _}) when via in [:sms, "sms", :call, "call"] do
+    Authy.post(@base_url <> "/start", params)
+  end
+
   defp set_defaults(params) do
     Application.get_env(:authy, :phone_verification, [])
     |> Dict.take([:via, :country_code, :locale, :custom_message])
--- test/authy/phone_verification_test.exs
+++ test/authy/phone_verification_test.exs
@@ -1,4 +1,23 @@
 defmodule Authy.PhoneVerificationTest do
-  use ExUnit.Case, async: true
-  doctest Authy.PhoneVerification
+  use ExUnit.Case, async: false
+  import Authy.PhoneVerification
+
+  setup_all do
+    defaults = [via: "sms", country_code: "61"]
+    Application.put_env(:authy, :phone_verification, defaults)
+    on_exit fn -> Application.delete_env(:authy, :phone_verification) end
+    :ok
+  end
+
+  test "start calls post_fn with given params" do
+    params = %{via: "call", phone_number: "0477777777", country_code: "44"}
+    assert_params = fn (p) -> assert p == params end
+    start(params, assert_params)
+  end
+
+  test "start pulls defaults from settings" do
+    params = %{phone_number: "0477777777"}
+    assert_params = fn (p) -> assert p == %{via: "sms", phone_number: "0477777777", country_code: "61"} end
+    start(params, assert_params)
+  end
 end
