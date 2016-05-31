b5b20446ef2c7662ac5e6dcd1bc9ba4127fb8b5f
Brenton Annan <brentonannan@brentonannan.com>
2016-05-31 12:41:09 +1000

    Define Authy.Helpers module

---
+++ lib/authy/helpers.ex
@@ -0,0 +1,5 @@
+defmodule Authy.Helpers do
+  @moduledoc """
+  Helpers for working with Authy requests and responses
+  """
+end
---
+++ test/authy/helpers_test.exs
@@ -0,0 +1,4 @@
+defmodule Authy.HelpersTest do
+  use ExUnit.Case, async: true
+  doctest Authy.Helpers
+end
