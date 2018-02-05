9f1d0368a307165b48cb8a63be54563e49fedf1a
Jose Sebio <jalopezsebio@gmail.com>
2018-02-06 10:41:16 +1100

    Apply elixir formatter

--- lib/authy/helpers.ex
+++ lib/authy/helpers.ex
@@ -12,7 +12,7 @@ defmodule Authy.Helpers do
   iex> Authy.Helpers.parse_response(%HTTPoison.Response{body: %{"success" => false, "message" => "Phone verification couldn't be created: Phone number is invalid"}})
   {:error, "Phone verification couldn't be created: Phone number is invalid"}
   """
-  @spec parse_response(HTTPoison.Response.t) :: Authy.response
+  @spec parse_response(HTTPoison.Response.t()) :: Authy.response()
   def parse_response(%HTTPoison.Response{body: %{"success" => true, "message" => message}}) do
     {:ok, message}
   end
